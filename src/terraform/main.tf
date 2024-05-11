65ef82d39fd4cd0ab96410516f0c50e7b2dbb515
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-05-12 03:34:30 +0530

    Update terraform with portainer

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,17 +2,25 @@
 resource "docker_image" "jenkins_image" {
   name = "jenkins/jenkins:lts"
 }
+resource "docker_image" "portainer_image" {
+  name = "portainer/portainer-ce:latest"
+}
+
 
 # Create volume
 resource "docker_volume" "jenkins_volume" {
   name = "jenkins_data"
 }
+resource "docker_volume" "portainer_volume" {
+  name = "portainer_data"
+}
+
 
 # Create a container
 resource "docker_container" "jenkins_container" {
-  image     = docker_image.jenkins/jenkins.name
+  image     = docker_image.jenkins_image.name
   name      = "jenkins"
-  restart   = "unless stopped"
+  restart   = "unless-stopped"
   volumes {
     volume_name    = docker_volume.jenkins_volume.name
     container_path = "/var/jenkins"
@@ -20,5 +28,26 @@ resource "docker_container" "jenkins_container" {
   ports {
     internal = 8080
     external = 8080
+  }
+}
+resource "docker_container" "portainer_container" {
+  image     = docker_image.portainer_image.name
+  name      = "portainer"
+  restart   = "always"
+  ports {
+    internal = 8000
+    external = 8000
+  }
+  ports {
+    internal = 9443
+    external = 9443
+  }
+  volumes {
+    volume_name    = docker_volume.portainer_volume.name
+    container_path = "/data"
+  }
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
   }
 }
